35af2448bc6571eb37e74bad18b0ca6dbc0c7335
MATHEUS EGON SCHNEIDER <matheus_schneider150@estudante.sesisenai.org.br>
2026-03-12 19:22:36 -0300

    V-1.0

--- main.cpp
+++ main.cpp
@@ -1,18 +1,109 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <LiquidCrystal.h>
+#include <EEPROM.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// LCD (RS, E, D4, D5, D6, D7)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// Sensores de nível
+const int sensorNivel[5] = {6,7,8,9,10};
+
+// Altura correspondente em cm
+const int alturaNivel[5] = {2,4,6,8,9};
+
+// Sensor de vazão
+const int sensorVazao = A0;
+
+// Relé da bomba
+const int bomba = 13;
+
+// Variáveis do sistema
+int alturaAtual = 0;
+unsigned long consumoTotal = 0;
+
+void setup()
+{
+  lcd.begin(16,2);
+
+  for(int i=0;i<5;i++)
+  {
+    pinMode(sensorNivel[i], INPUT);
+  }
+
+  pinMode(bomba, OUTPUT);
+
+  // Recupera consumo salvo
+  EEPROM.get(0, consumoTotal);
+
+  lcd.print("Sistema Agua");
+  delay(2000);
+  lcd.clear();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  lerNivel();
+  controlarBomba();
+  calcularConsumo();
+  mostrarLCD();
+
+  delay(500);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// -------------------------
+
+void lerNivel()
+{
+  for(int i=4;i>=0;i--)
+  {
+    if(digitalRead(sensorNivel[i]) == HIGH)
+    {
+      alturaAtual = alturaNivel[i];
+      break;
+    }
+  }
+}
+
+// -------------------------
+
+void controlarBomba()
+{
+  if(alturaAtual <= 4)
+  {
+    digitalWrite(bomba, HIGH);
+  }
+  else if(alturaAtual >= 9)
+  {
+    digitalWrite(bomba, LOW);
+  }
+}
+
+// -------------------------
+
+void calcularConsumo()
+{
+  int leitura = analogRead(sensorVazao);
+
+  int fluxo = map(leitura,0,1023,0,10);
+
+  consumoTotal += fluxo;
+
+  EEPROM.put(0, consumoTotal);
+}
+
+// -------------------------
+
+void mostrarLCD()
+{
+  lcd.setCursor(0,0);
+  lcd.print("Nivel:");
+  lcd.print(alturaAtual);
+  lcd.print("cm   ");
+
+  lcd.setCursor(0,1);
+
+  lcd.print("Cons:");
+  lcd.print(consumoTotal);
+  lcd.print(" L   ");
 }
